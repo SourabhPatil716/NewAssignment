a42f67021dd5aaef74e777c76320170193121c71
SourabhPatil716 <sourabhmahadik397@gmail.com>
2025-03-29 15:18:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function updateTotal(value) {
+  const totalElement = document.getElementById("total");
+  totalElement.textContent = `$ ${value.toFixed(2)} USD`;
+}
+
+function addToCart() {
+  const selectedOption = document.querySelector('input[name="offer"]:checked');
+  if (selectedOption) {
+    alert(`Added ${selectedOption.id} to cart with a total of $${selectedOption.value} USD.`);
+  } else {
+    alert("Please select an option before adding to the cart.");
+  }
+}
